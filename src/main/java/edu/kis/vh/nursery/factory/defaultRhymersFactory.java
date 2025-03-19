0dac38011a9a9c42e9ad216adbe4f264a885023a
haslotomaslo <251148@edu.p.lodz.pl>
2025-03-19 21:25:42 +0100

    5. poprawa nazw pól

--- src/main/java/edu/kis/vh/nursery/factory/defaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/defaultRhymersFactory.java
@@ -1,11 +1,10 @@
 package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRhymer;
-import edu.kis.vh.nursery.FIFORhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
-import edu.kis.vh.nursery.factory.Rhymersfactory;
+import edu.kis.vh.nursery.fiforhymer;
+import edu.kis.vh.nursery.hanoirhymer;
 
-public class DefaultRhymersFactory implements Rhymersfactory {
+public class defaultRhymersFactory implements rhymersfactory {
 
     @Override
     public defaultCountingOutRhymer GetStandardRhymer() {
@@ -19,12 +18,12 @@ public class DefaultRhymersFactory implements Rhymersfactory {
 
     @Override
     public defaultCountingOutRhymer GetFIFORhymer() {
-        return new FIFORhymer();
+        return new fiforhymer();
     }
 
     @Override
     public defaultCountingOutRhymer GetHanoiRhymer() {
-        return new HanoiRhymer();
+        return new hanoirhymer();
     }
 
 }
--- src/main/java/edu/kis/vh/nursery/fiforhymer.java
+++ src/main/java/edu/kis/vh/nursery/fiforhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class fiforhymer extends defaultCountingOutRhymer {
 
     public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 
