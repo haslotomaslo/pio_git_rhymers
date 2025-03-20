bd4b8e611920df977018162e51cc7bd77da46045
janslu <252832@edu.p.lodz.pl>
2025-03-20 17:00:44 +0100

    8. Added override property to hanoyrhymer methods

--- src/main/java/edu/kis/vh/nursery/hanoirhymer.java
+++ src/main/java/edu/kis/vh/nursery/hanoirhymer.java
@@ -4,11 +4,12 @@ package edu.kis.vh.nursery;
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
     int totalRejected = 0;
-
+    @OverRide
     public int reportRejected() {
         return totalRejected;
     }
 
+    @OverRide
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
