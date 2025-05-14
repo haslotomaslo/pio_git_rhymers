662fe95ee7896593590ad727a3d7bbd8ad28233c
haslotomaslo <251148@edu.p.lodz.pl>
2025-05-14 20:36:12 +0200

    Poprawiony DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,15 +3,18 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
     private static final int MAX_SIZE = 12;
-    private final int[] NUMBERS = new int[MAX_SIZE];
-    final private int EMPTY = -1;
-    final private int DEFAULT_RETURN = -1;
+    private static final int EMPTY = -1;
+    private static final int DEFAULT_RETURN = -1;
+    private final int[] numbers = new int[MAX_SIZE];
 
     private int total = -1;
+	public int getTotal() {
+        return total;
+    }
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbers[++total] = in;
     }
 
     public boolean callCheck() {
@@ -25,13 +28,13 @@ public class DefaultCountingOutRhymer {
     protected int peekaboo() {
         if (callCheck())
             return DEFAULT_RETURN;
-        return NUMBERS[total];
+        return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
             return DEFAULT_RETURN;
-        return NUMBERS[total--];
+        return numbers[total--];
     }
 
 }
